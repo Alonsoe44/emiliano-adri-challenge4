7cd1c82c8e8d6eb1a58b1ebf27510a8a18ac4a53
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-01-20 19:30:27 +0100

    Baby steps on push method

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// hi
+class SkylabArray {
+  elements;
+  length;
+
+  constructor() {
+    this.elements = {};
+  }
+
+  pushElement(input) {
+    this.elements[input] = 1;
+  }
+}
+
+const superArray = new SkylabArray();
+
+superArray.pushElement("friends");
+superArray.pushElement("cebra");
+
+console.log(superArray);
